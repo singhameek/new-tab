2aa94caf62043fc808991f63861d6cab52d97fcb
singhameek <ameeksinghv@proton.me>
2026-08-21 22:35:53 +0000

    search bar working, added RTC

---
+++ script.js
@@ -0,0 +1,27 @@
+function updateClock() {
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    document.getElementById('clock').innerText = `${hours}:${minutes}:${seconds}`
+
+}
+
+updateClock();
+setInterval(updateClock, 1000);
+
+const searchInput = document.getElementById('input');
+
+searchInput.addEventListener('keydown', function(event) {
+
+    if (event.key === 'Enter') {
+        const query = searchInput.value.trim();
+        if (query != ''){
+            window.location.href = `https://google.com/search?q=${encodeURIComponent(query)}`;
+        }
+    }
+});
